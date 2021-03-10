c9d1f6b8b12e1ad0357eddf0601e892f6486378d
jglez <jglez.cs@gmail.com>
2021-03-09 23:05:41 -0600

    Added endpoint for find() helper function to get all posts

--- api/server.js
+++ api/server.js
@@ -1,2 +1,25 @@
-// implement your server here
-// require your posts router and connect it here
+// Import Express from node_modules
+const express = require('express')
+
+// Import database access helper functions
+const Post = require('./posts/posts-model')
+
+// Instantiate Server
+const server = express()
+
+// Teach Express how to parse JSON bodies
+server.use(express.json())
+
+/***** ENDPOINTS *****/
+server.get('/api/posts', (req, res) => {
+  Post.find()
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message })
+    })
+})
+
+// Expose to the outer world
+module.exports = server
